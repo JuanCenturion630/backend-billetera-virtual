8e16fbed9bb750fac5171dd7daf34dd7cc10cfc5
JuanCenturion630 <centurion.juan128@gmail.com>
2024-06-05 05:06:14 -0300

    Desarrollo del módulo transaccione - No se consiguió Cognito

--- routes/usuariosRoutes.js
+++
@@ -1,9 +0,0 @@
-//Rutas para el manejo de solicitudes HTTP:
-
-const express = require('express');
-const router = express.Router();
-const usuariosController = require('../controllers/usuariosController');
-
-router.post('/', usuariosController.crearUsuario); //Ruta para crear un nuevo usuario.
-
-module.exports = router;
